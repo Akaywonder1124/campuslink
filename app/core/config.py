7f7f0032546ebc65536b67829ee219a4a114e718
Abdulakeem Asiru <97950441+Akaywonder1124@users.noreply.github.com>
2023-06-23 10:19:06 +0100

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api_v1"
-    SECRET_KEY = '4cd339bb2b00f49b8a9fd0a4346044d9'
+    SECRET_KEY = ''
     ACCESS_TOKEN_EXPIRE_MINUTES  :int = 40
     MEMBER = "member"
     ADMIN_ROLE = "admin"
